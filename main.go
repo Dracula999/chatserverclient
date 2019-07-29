d93a128798cb833fa7b50fe24f12eb39af748df5
Ulugbek Babazhanov <ulugbek.babazhanov@btsdigital.kz>
2019-07-29 15:26:55 +0600

    fds

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"github.com/Dracula999/chatserverclient"
+	"github.com/gorilla/websocket"
+)
+
+type ClientManager struct {
+	clients    map[*Client]bool
+	broadcast  chan []byte
+	register   chan *Client
+	unregister chan *Client
+}
+
+type Client struct {
+	id     string
+	socket *websocket.Conn
+	send   chan []byte
+}
+
+type Message struct {
+	Sender    string `json:"sender,omitempty"`
+	Recipient string `json:"recipient,omitempty"`
+	Content   string `json:"content,omitempty"`
+}
+
+var manager = ClientManager{
+	broadcast:  make(chan []byte),
+	register:   make(chan *Client),
+	unregister: make(chan *Client),
+	clients:    make(map[*Client]bool)
+}
+
+func main() {
+	fmt.Println("Hello")
+}
---
+++ models.go
@@ -0,0 +1,7 @@
+package main
+
+type Client struct {
+	name     string
+	id       string
+	username string
+}
